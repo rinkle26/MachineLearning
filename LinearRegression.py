cf00178a77f9f48e763092c4a8132a85bd8e8c49
Rinkle Seth <rcs170004@utdallas.edu>
2019-02-17 16:07:21 -0600

    Create LinearRegression.py
    -Generating synthetic data with some Gaussian Noise.
    -Creating a linear regression model to predict future values by training it on the sample data which we generated.
    -Plotting the model to see it's error rate and how well it generalizes.

---
+++ LinearRegression.py
@@ -0,0 +1,41 @@
+#GENERATING SYNTHETIC DATA
+import numpy as np
+def f_true(x):
+  y = 6.0 * (np.sin(x + 2) + np.sin(2*x + 4))
+  return y
+  
+# Data with Gaussian noise.  
+import numpy as np                       # For all our math needs
+n = 750                                  # Number of data points
+X = np.random.uniform(-7.5, 7.5, n)      # Training examples, in one dimension
+e = np.random.normal(0.0, 5.0, n)        # Random Gaussian noise
+y = f_true(X) + e  
+  
+#Plotting 
+import matplotlib.pyplot as plt          # For all our plotting needs
+plt.figure()
+
+# Plot the data
+plt.scatter(X, y, 12, marker='o')           
+
+# Plot the true function, which is really "unknown"
+x_true = np.arange(-7.5, 7.5, 0.05)
+y_true = f_true(x_true)
+plt.plot(x_true, y_true, marker='None', color='r')  
+
+#Training the model and performing testing and validation.
+from sklearn.model_selection import train_test_split
+tst_frac = 0.3  # Fraction of examples to sample for the test set
+val_frac = 0.1  # Fraction of examples to sample for the validation set
+
+# First, we use train_test_split to partition (X, y) into training and test sets
+X_trn, X_tst, y_trn, y_tst = train_test_split(X, y, test_size=tst_frac, random_state=42)
+
+# Next, we use train_test_split to further partition (X_trn, y_trn) into training and validation sets
+X_trn, X_val, y_trn, y_val = train_test_split(X_trn, y_trn, test_size=val_frac, random_state=42)
+
+# Plot the three subsets
+plt.figure()
+plt.scatter(X_trn, y_trn, 12, marker='o', color='orange')
+plt.scatter(X_val, y_val, 12, marker='o', color='green')
+plt.scatter(X_tst, y_tst, 12, marker='o', color='blue')
